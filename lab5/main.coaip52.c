eb04454808466e40711ae7fbc2b6b655ffbe45e9
shvarovadasha <shvarovadasha28@gmail.com>
2022-12-04 18:19:22 +0300

    OAiP

--- lab5/main.coaip52.c
+++ lab5/main.coaip52.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main(){
 	int a[3][3];
-	int i, j, p;
+	int i, j, p, sum;
 	for(i=0; i<3; i++)
 	for(j=0; j<3; j++)
 	{
@@ -23,7 +23,29 @@ int main(){
 		   printf("Proizvedenie elementov %d-stroki matrici = %.2d\n", i+1, p);
     }}
     
-    
+    printf ("Cymmi elementov diagonaley matrici, paralel'nie glavnoy: \n");
+ for (j=0;j<(2*(3-1));j++) sum[j]=0;
+  for (i=0;i<3;i++)
+  {
+      for (j=0;j<3;j++)
+      {
+          if(i!=j)
+          {
+              if(i>j)
+                  sum[3-1-i+j]+=a[i][j];
+              else
+                  sum[3+j-i-2]+=a[i][j]; 
+          }
+      }      
+  }
+  for (j=0;j<2*(3-1);j++) printf("%d\n",sum[j]);  
+  maxsum=-1000;
+  for (j=0;j<2*(3-1);j++)
+      if (maxsum<s[j]) 
+          maxsum=s[j];
+  printf("\n");
+  printf("Max sum = %d\n",maxsum);
+  getch();
     
     
     
