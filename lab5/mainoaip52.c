7964563833ab190413cd4d9252f8c1bf6f179a77
shvarovadasha <shvarovadasha28@gmail.com>
2022-12-22 20:13:13 +0300

    OAiP

--- lab5/mainoaip52.c
+++ lab5/mainoaip52.c
@@ -3,7 +3,8 @@
 #include <conio.h>
 #define n 3
 #define m 3
-int main() {
+int main()
+{
 	int a[n][m];
 	int i, j, p;
 	 for (i = 0; i < n; i++) { 
@@ -27,27 +28,28 @@ int main() {
 		 printf (" The product of array elements in %d-row that do not contain negative elements: %.d\n", i + 1, p); 
 	  }
     }
-	//нахождение максимума среди сумм элементов диагоналей, параллельных главной диагонали матрицы	
-	int maxsum = 0, sum= 0, k;
-	  for(k = 0; k < ; k++)
-    {
-        i = k < SZ - 1 ? 0 : index_i++;
-        j = k < SZ - 1 ? index_j-- : 0;
-        if(k >= SZ - 1)     size--;
-        while(j < size)
-            sum[k] += matr[i++][j++];
-        if(sum[k] > max_sum)
-            max_sum = sum[k];
-    }
-         printf (" The max sum of diagonal elements which are parallel to main diagonal: %.d\n", maxsum);
-    }
- 
-	  	
-	 
-
-	
-	
-	
-	
+    //нахождение максимума среди сумм элементов диагоналей, параллельных главной диагонали матрицы	
+    int sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0, maxsum = 0;
+    for (i = 0; i < n; i++) {
+    	for (j = 0; j < m; j++) {
+    		sum1 = a[0][2];
+    		sum2 = a[0][1] + a[1][2];
+    		sum3 = a[1][0] + a[2][1];
+    		sum4 = a[2][0];
+    		if (sum1 > maxsum) {
+    			maxsum = sum1;
+			}
+    		 else if (sum2 > maxsum) {
+    		 	maxsum = sum2;
+			 }
+			  else if (sum3 > maxsum) {
+			  	maxsum = sum3;
+			  }
+			   else if (sum4 > maxsum) {
+			   	maxsum = sum4;
+			   }
+		}
+	} 
+	printf (" The max sum of diagonal elements which are parallel to main diagonal: %.d\n", maxsum);	
 return 0;
 }
