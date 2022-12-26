454c70ad0b2b1b03c99da043be6db3f37c30cd2d
shvarovadasha <shvarovadasha28@gmail.com>
2022-12-26 18:06:29 +0300

    OAiP

--- lab5/mainoaip51.c
+++ lab5/mainoaip51.c
@@ -3,6 +3,7 @@
 int main() {
 	int a[3][3];
 	int i, j;
+	int flag[3] = {0, 0, 0};
 	 for (i = 0; i < 3; i++) { 
 	  for (j = 0; j < 3; j++) {
 		printf ("Vvedite element [%d,%d]\n", i + 1, j + 1); //ввод матрицы
@@ -15,15 +16,29 @@ int main() {
 	  } //вывод матрицы
 	    printf("\n"); 
 	 }
-	for ( int i = 0; i < 3; i++) {
+	 for ( int i = 0; i < 3; i++) {
 	 for( int j = 0; j < 3; j++) {
 	   if (a[i][j] <  0)
 		{
-          printf (" The row - %d of the array contains at least one negative element \n", i+1);
+          flag[i] = 1;
+          break;
         }
-	  a[i][j] = -a[i][j]; 
 	 }
     }
+    int sum = 0;
+	for ( int i = 0; i < 3; i++)
+		{
+          sum += flag[i];
+        }
+	printf ("The sum of flag array = %d\n", sum);
+	 
+	 if (sum == 3) {
+	 	for (i = 0; i < 3; i++) { 
+	     for(j = 0; j < 3; j++) { 
+	      a[i][j] = -a[i][j];
+	    }
+	     }
+	 }
 	for (i = 0; i < 3; i++) { 
 	 for(j = 0; j < 3; j++) { 
 	   printf(" %d ", a[i][j]);
