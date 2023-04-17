47bf9170633a6fe4fd841a9d8a52f4deac3a8a3c
shvarovadasha <shvarovadasha28@gmail.com>
2023-04-17 22:08:00 +0300

    OAiP

--- lab7/main.coaip71.c
+++ lab7/main.coaip71.c
@@ -1,97 +1,72 @@
-#include <stdio.h>
-#include <stdio.h>  
-#define SIZE 20
 //Найти в матрице первую строку, все элементы которой положительны, и
 //сумму этих элементов. Уменьшить все элементы матрицы на эту сумму.
-int a[SIZE][SIZE];
+#include <stdio.h>
 
-void main(void)
-{
-	int a[SIZE][SIZE];
-	int n, m;
-	printf("Enter n\n");
-	scanf("%d", &n);
-	printf("Enter m\n");
-	scanf("%d", &m);
-	
-	input(a, n, m);
-	output(a, n,m);
-	
-	printf("\n");
-	
-	for (int i = 0; i < n; ++i)
-	{
-		if (search(a[i], n))
-		{
-		sum
-		break;
-	    }
+#define n 3
+#define m 3
+
+void findFirstRowAndReduce(int matrix[n][m]) {
+    int sum = 0;
+    int found = 0;
+    
+    for (int i = 0; i < n; i++) {
+        int allPositive = 1;
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] <= 0) {
+                allPositive = 0;
+                break;
+            }
+        }
+        
+        if (allPositive) {
+            found = 1;
+            printf("First row with all positive elements: Row %d\n", i + 1);
+            for (int j = 0; j < m; j++) {
+                sum += matrix[i][j];
+            }
+            break;
+        }
     }
     
-    for (int i = 0; i < n; i++)
-	{
-    	for (int j = 0; j < n; j++)
-    	   printf("%d", a[i][j]);
-    	   printf("\n");
-	}
+    if (found) {
+        printf("Sum of positive elements: %d\n", sum);
+        printf("Reducing all elements by %d\n", sum);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] -= sum;
+            }
+        }
+        printf("Matrix after reduction:\n");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                printf("%d ", matrix[i][j]);
+            }
+            printf("\n");
+        }
+    } else {
+        printf("No row found with all positive elements.\n");
+    }
 }
 
-void input(int a[SIZE][SIZE], int n1, int m1)  //запись элементов массива
-{
-	int i, j;
-	for (i = 0; i < n1; i++)
-	{
-		for (j = 0; j < m1; j++)
-		{
-			printf("Enter element [%d,%d]: ", i + 1, j + 1);
-			scanf("%d", &a[i][j]);
-		}
-	}
+int main() {
+    int matrix[n][m] = {
+        {1, 2, 3},
+        {-4, 5, -6},
+        {7, 8, 9}
+    };
+    
+    printf("Original matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    
+    findFirstRowAndReduce(matrix);
+    
+    return 0;
 }
-
-int output(int a[SIZE][SIZE], int n, int m)  //вывод на экран элементов массива
-{
-	printf("\n");
-	int i, j;
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < m; j++)
-		{
-			printf("%d ", a[i][j]);
-		}
-	printf("\n");
-	}
-	return 0;
-}
-
-int search(int a[SIZE][SIZE], int n, int m) 
-{
-	int i = 0;
-	int j = 0;
-	int k = 0;
-	int index = 0;
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < m; j++)
-		{
-			if (a[i][j] > 0)
-			k ++;
-			else
-			{
-				k = 0;
-				break;
-			}
-		}
-		if (k == n)
-		{
-			index = i + 1;
-			break;
-		}
-	printf("\n");
-	}
-	return 0;
-}
-
 
 
 
