34806bfbdefa4f46ac8112bc9b853e96ff27b2bb
shvarovadasha <shvarovadasha28@gmail.com>
2023-06-19 10:47:09 +0300

    OAiP

--- lab8/main.coaip8.c
+++ lab8/main.coaip8.c
@@ -1,239 +1,199 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-// Ввести массив структур в соответствии с вариантом. Рассортировать массив в
-// алфавитном порядке по первому полю, входящему в структуру. В программе реализовать меню:
-// 1) Ввод массива структур;
-// 2) Сортировка массива структур;
-// 3) Поиск в массиве структур по заданному параметру;
-// 4) Изменение заданной структуры;
-// 5) Удаление структуры из массива;
-// 6) Вывод на экран массива структур;
-// 7) Выход.
-// 9. Структура «Вокзал»: номер поезда, пункт назначения, дни следования, время прибытия,
-// время стоянки.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-struct station //структура для вокзала
-{
-	int trainNumber;
-	char destination[20];
-	char timetable[20];
-	int arrivalTime;
-	int stoppingTime;
+#define MAX_TRAINS 100  // Максимальное количество поездов
+
+// Структура "Вокзал"
+struct TrainStation {
+    int trainNumber;
+    char destination[100];
+    char daysOfOperation[50];
+    char arrivalTime[10];
+    char parkingTime[10];
 };
 
-struct station trains[5], temp;	//массивы структур
-int n = 0;	//переменная, хранящая количество поездов 
-
-
-void addTrain (void) //ввод массива структур
-{
-	system("cls");
-	fflush(stdin);
-	if (n < 5)
-	{
-		printf("Train %d\n", n + 1);
-		
-		fputs("Enter train number: ", stdout);
-		fflush(stdin);
-		scanf("%d", &trains[n].trainNumber);
-
-		fputs("Enter destination: ", stdout);
-		fflush(stdin);
-		fgets(trains[n].destination, 20, stdin);
-
-		fputs("Enter timetable: ", stdout);
-		fflush(stdin);
-		fgets(trains[n].timetable, 20, stdin);
-	
-		fputs("Enter arrival time: ", stdout);
-		fflush(stdin);
-		scanf("%d", &trains[n].arrivalTime);
-	
-		fputs("Enter stopping time: ", stdout);
-		fflush(stdin);
-		scanf("%d", &trains[n].stoppingTime);
-
-		n++;
-	}
-	else
-	{
-		printf("The max number of trains cannot be more than 5\n");
-	}
-	
-	printf("Enter 0 to return: "); //чтобы вернуться назад нажмите 0
-    int i;
-    scanf("%d", &i);
-    if(i == 0)
-	{
-        return;
+// Функция для ввода массива структур
+void inputTrains(struct TrainStation trains[], int size) {
+    printf("Enter information about the trains:\n");
+    for (int i = 0; i < size; i++) {
+        printf("Train %d:\n", i + 1);
+        printf("Train number: ");
+        scanf("%d", &trains[i].trainNumber);
+        printf("Destination: ");
+        fflush(stdin);  // Очищаем буфер ввода перед считыванием строки
+        fgets(trains[i].destination, sizeof(trains[i].destination), stdin);
+        trains[i].destination[strcspn(trains[i].destination, "\n")] = '\0';  // Удаляем символ новой строки из ввода
+        printf("Days of operation: ");
+        fflush(stdin);
+        fgets(trains[i].daysOfOperation, sizeof(trains[i].daysOfOperation), stdin);
+        trains[i].daysOfOperation[strcspn(trains[i].daysOfOperation, "\n")] = '\0';
+        printf("Arrival time: ");
+        fflush(stdin);
+        fgets(trains[i].arrivalTime, sizeof(trains[i].arrivalTime), stdin);
+        trains[i].arrivalTime[strcspn(trains[i].arrivalTime, "\n")] = '\0';
+        printf("Parking time: ");
+        fflush(stdin);
+        fgets(trains[i].parkingTime, sizeof(trains[i].parkingTime), stdin);
+        trains[i].parkingTime[strcspn(trains[i].parkingTime, "\n")] = '\0';
+        printf("\n");
     }
 }
 
-void searchTrain(void)  //Поиск в массиве структур по заданному параметру
-{
-	system("cls");  
-	fflush(stdin);
-	int trainNumber;
-	printf("Enter train number: ");
-	scanf("%d", &trainNumber);
-	for(int i = 0; i < 5; i++)
-	{
-		if(strcmp(trains == trainNumber) )
-		{
-			printf("Available information: \n");
-			printf("Train number: %d", trains[i].trainNumber);
-			printf("Destination: %s", trains[i].destination);
-			printf("Timetable: %s", trains[i].timetable);
-			printf("Arrival time: %d", trains[i].arrivalTime);
-			printf("\nStopping time: %d\n", trains[i].stoppingTime);
-		}
-	}
-	printf("Enter 0 to return: ");
-	int i;
-	scanf("%d", &i);
-	if(i == 0){
-		return;
-	}
+// Функция для сортировки массива структур по номеру поезда
+void sortTrains(struct TrainStation trains[], int size) {
+    struct TrainStation temp;
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (trains[i].trainNumber > trains[j].trainNumber) {
+                temp = trains[i];
+                trains[i] = trains[j];
+                trains[j] = temp;
+            }
+        }
+    }
+    printf("Array of structures sorted by train number.\n");
 }
 
-void editTrain(void)  //Изменение заданной структуры
-{
-	system("cls");  
-	fflush(stdin);
-	int num;   
-	printf("Enter train's number to edit: ");
-	scanf("%d", &num);
-	if(num > n)
-	{
-		return;
-	}
-	getchar();
-	
-	printf("Enter train number: ", stdout);
-	scanf("%d", &trains->trainNumber);
-
-	fputs("Enter destination: ", stdout);
-	fgets(trains->destination, 20, stdin);
-
-	fputs("Enter timetable: ", stdout);
-	fgets(trains->timetable, 20, stdin);
-	
-	fputs("Enter arrival time: ", stdout);
-	scanf("%d", &trains->arrivalTime);
-	
-	fputs("Enter stopping time: ", stdout);
-	scanf("%d", &trains->stoppingTime);
-	
-}
-
-void deleteTrain(void) //Удаление структуры из массива
-{ 
-	system("cls");  
-	fflush(stdin);
-	int num;
-	printf("Enter train number to delete: ");
-	scanf("%d", &num);
-	if(num > n)
-	{	
-		return;
-	}
-	printf("Are you sure you want to delete this train? (0 - No, 1 - Yes): ");
-	int i;
-	scanf("%d", &i);
-	if(i != 0)
-	{
-		for(int i = (num - 1); i < n; i++)
-		{
-			trains[i] = trains[i + 1];
-		}
-		n--;
-	}
-}
-
-
-void showAllTrains(void)  //Вывод на экран массива структур
-{
-	system("cls");  
-	fflush(stdin);
-	
-	if(n == 0)
-	{
-		printf("Nothing is recorded");
-	}
-	
-
-	for(int i = 0; i < n - 1; i++) //Сортировка массива структур
-	{
-		for(int j = i + 1; j < n; j++)
-		{
-			if(strcmp(trains[i].trainNumber, trains[j].trainNumber) > 0){
-				temp = trains[i];
-				trains[i] = trains[j];
-				trains[j] = temp;
-			}
-		}
-	}
-	
-	for(int i = 0; i < n; i++)
-	{
-		printf("Train %d\n", i + 1);
-		printf("Enter train number: %d\n", trains[i].trainNumber);
-		printf("Enter destination: %s", trains[i].destination);
-		printf("Enter timetable: %s", trains[i].timetable);
-		printf("Enter arrival time: %d", trains[i].arrivalTime);
-		printf("\nEnter stopping time: %d\n", trains[i].stoppingTime);
-	}
-	
-	printf("\nEnter 0 to return: ");
-    int i;
-    scanf("%d", &i);
-    if(i == 0)
-	{
-        return;
+// Функция для поиска структуры по номеру поезда
+void searchTrain(struct TrainStation trains[], int size, int trainNumber) {
+    int found = 0;
+    for (int i = 0; i < size; i++) {
+        if (trains[i].trainNumber == trainNumber) {
+            printf("Train with number %d:\n", trainNumber);
+            printf("Destination: %s\n", trains[i].destination);
+            printf("Days of operation: %s\n", trains[i].daysOfOperation);
+            printf("Arrival time: %s\n", trains[i].arrivalTime);
+            printf("Parking time: %s\n", trains[i].parkingTime);
+            printf("\n");
+            found = 1;
+            break;
+        }
+    }
+    if (!found) {
+        printf("Train with number %d not found.\n", trainNumber);
     }
 }
 
-void menu(void){
-		int isFound;
-		while(1){
-		system("cls");
-		fflush(stdin);
-		int trainNumber;
-		printf("1.Add train\n");
-		printf("2.Search train\n");
-		printf("3.Edit train\n");
-		printf("4.Delete train\n");
-		printf("5.Show all trains\n");
-		printf("6.Exit\n");
-		printf("Select number: ");
-		int i;
-		scanf("%d", &i);
-		getchar();
-		switch(i)
-		{
-			case 1:	
-				addTrain();
-				break;
-			case 2:
-				searchTrain();
-				break;
-			case 3:
-				editTrain();
-				break;
-			case 4:
-				deleteTrain();
-				break;
-			case 5:
-				showAllTrains();
-				break;
-			case 6:
-				return;
-		}
-	}
+// Функция для изменения структуры по номеру поезда
+void modifyTrain(struct TrainStation trains[], int size, int trainNumber) {
+    int found = 0;
+    for (int i = 0; i < size; i++) {
+        if (trains[i].trainNumber == trainNumber) {
+            printf("Enter new information for train with number %d:\n", trainNumber);
+            printf("New destination: ");
+            fflush(stdin);
+            fgets(trains[i].destination, sizeof(trains[i].destination), stdin);
+            trains[i].destination[strcspn(trains[i].destination, "\n")] = '\0';
+            printf("New days of operation: ");
+            fflush(stdin);
+            fgets(trains[i].daysOfOperation, sizeof(trains[i].daysOfOperation), stdin);
+            trains[i].daysOfOperation[strcspn(trains[i].daysOfOperation, "\n")] = '\0';
+            printf("New arrival time: ");
+            fflush(stdin);
+            fgets(trains[i].arrivalTime, sizeof(trains[i].arrivalTime), stdin);
+            trains[i].arrivalTime[strcspn(trains[i].arrivalTime, "\n")] = '\0';
+            printf("New parking time: ");
+            fflush(stdin);
+            fgets(trains[i].parkingTime, sizeof(trains[i].parkingTime), stdin);
+            trains[i].parkingTime[strcspn(trains[i].parkingTime, "\n")] = '\0';
+            printf("Train with number %d successfully modified. \n", trainNumber);
+            found = 1;
+            break;
+        }
+    }
+    if (!found) {
+        printf("Train with number %d not found.\n", trainNumber);
+    }
 }
 
-int main(void)
-{
-	menu();
+// Функция для удаления структуры по номеру поезда
+void deleteTrain(struct TrainStation trains[], int *size, int trainNumber) {
+    int found = 0;
+    for (int i = 0; i < *size; i++) {
+        if (trains[i].trainNumber == trainNumber) {
+            for (int j = i; j < *size - 1; j++) {
+                trains[j] = trains[j + 1];
+            }
+            (*size)--;
+            printf("Train with number %d successfully deleted.\n", trainNumber);
+            found = 1;
+            break;
+        }
+    }
+    if (!found) {
+        printf("Train with number %d not found.\n", trainNumber);
+    }
+}
+
+// Функция для вывода массива структур
+void displayTrains(struct TrainStation trains[], int size) {
+    printf("Array of structures:\n");
+    for (int i = 0; i < size; i++) {
+        printf("Train %d:\n", i + 1);
+        printf("Train number: %d\n", trains[i].trainNumber);
+        printf("Destination: %s\n", trains[i].destination);
+        printf("Days of operation: %s\n", trains[i].daysOfOperation);
+        printf("Arrival time: %s\n", trains[i].arrivalTime);
+        printf("Parking time: %s\n", trains[i].parkingTime);
+        printf("\n");
+    }
+}
+
+int main() {
+    struct TrainStation trains[MAX_TRAINS];
+    int size = 0;
+    int choice, trainNumber;
+
+    do {
+        printf("Menu:\n");
+        printf("1) Input an array of structures\n");
+        printf("2) Sort the array of structures\n");
+        printf("3) Search in the array of structures by train number\n");
+        printf("4) Modify a structure by train number\n");
+        printf("5) Delete a structure from the array\n");
+        printf("6) Display the array of structures\n");
+        printf("7) Exit\n");
+        printf("Choose an operation: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter the number of trains: ");
+                scanf("%d", &size);
+                inputTrains(trains, size);
+                break;
+            case 2:
+                sortTrains(trains, size);
+                break;
+            case 3:
+                printf("Enter the train number to search: ");
+                scanf("%d", &trainNumber);
+                searchTrain(trains, size, trainNumber);
+                break;
+            case 4:
+                printf("Enter the train number to modify: ");
+                scanf("%d", &trainNumber);
+                modifyTrain(trains, size, trainNumber);
+                break;
+            case 5:
+                printf("Enter the train number to delete: ");
+                scanf("%d", &trainNumber);
+                deleteTrain(trains, &size, trainNumber);
+                break;
+            case 6:
+                displayTrains(trains, size);
+                break;
+            case 7:
+                printf("Program terminated.\n");
+                break;
+            default:
+                printf("Invalid operation choice. Please try again.\n");
+                break;
+        }
+
+        printf("\n");
+    } while (choice != 7);
+
+    return 0;
 }
